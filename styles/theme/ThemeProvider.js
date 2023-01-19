7787c45c114788d0de91700b8f8795cd5bfa40fe
Jonas Wouters <r0781280@student.thomasmore.be>
2023-01-19 14:35:14 +0100

    Theme

--- styles/theme/ThemeProvider.js
+++ styles/theme/ThemeProvider.js
@@ -6,7 +6,7 @@ import { colors } from './colors';
 export const ThemeContext = React.createContext();
 
 const ThemeProvider = ({children}) => {
-  const [isLightTheme, setLightTheme] = useState("0");
+  const [isLightTheme, setLightTheme] = useState("1");
 
   const getTheme = async () => {
     try {
